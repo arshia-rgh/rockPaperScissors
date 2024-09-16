af20bdbd8747f78e5ab30f2a2506ccf4908875fe
arshia <arshiarezagholi1212@gmail.com>
2024-09-16 22:39:54 +0330

    moved menu functions to the file menues

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/olekukonko/tablewriter"
-	"os"
 )
 
 type User struct {
@@ -14,41 +12,6 @@ type User struct {
 var rockPaperScissors = map[int]string{1: "rock", 2: "paper", 3: "scissors"}
 
 var users []User
-
-func main_menu() int {
-	fmt.Println("*****************************")
-	fmt.Println("**********Welcome************")
-	fmt.Println("Please enter what do you want to do: ")
-	fmt.Println("1 - Start the game")
-	fmt.Println("2 - Create new player")
-	fmt.Println("3 - Check Leaderboard ")
-	fmt.Println("4 - Exit...")
-	var option int
-
-	_, err := fmt.Scan(&option)
-	if err != nil {
-		return 0
-	}
-
-	return option
-}
-
-func start_game_menu() int {
-	fmt.Println("Welcome---")
-	fmt.Println("Please Enter your choice: ")
-	fmt.Println("1 - Play vs ai")
-	fmt.Println("2 - Play vs Player")
-	fmt.Println("3 - Back to main menu")
-	var option int
-
-	_, err := fmt.Scan(&option)
-
-	if err != nil {
-		return 0
-	}
-
-	return option
-}
 
 func create_new_player() {
 	var name string
@@ -77,16 +40,6 @@ func create_new_player() {
 	users = append(users, user)
 }
 
-func leaderboard_menu() {
-	table := tablewriter.NewWriter(os.Stdout)
-
-	table.SetHeader([]string{"Name", "Score"})
-	for _, user := range users {
-		table.Append([]string{user.name, fmt.Sprintf("%v", user.score)})
-	}
-
-	table.Render()
-}
 func main() {
 
 }
--- menu.go
+++ menu.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"os"
+)
+
+func main_menu() int {
+	fmt.Println("*****************************")
+	fmt.Println("**********Welcome************")
+	fmt.Println("Please enter what do you want to do: ")
+	fmt.Println("1 - Start the game")
+	fmt.Println("2 - Create new player")
+	fmt.Println("3 - Check Leaderboard ")
+	fmt.Println("4 - Exit...")
+	var option int
+
+	_, err := fmt.Scan(&option)
+	if err != nil {
+		return 0
+	}
+
+	return option
+}
+
+func start_game_menu() int {
+	fmt.Println("Welcome---")
+	fmt.Println("Please Enter your choice: ")
+	fmt.Println("1 - Play vs ai")
+	fmt.Println("2 - Play vs Player")
+	fmt.Println("3 - Back to main menu")
+	var option int
+
+	_, err := fmt.Scan(&option)
+
+	if err != nil {
+		return 0
+	}
+
+	return option
+}
+
+func leaderboard_menu() {
+	table := tablewriter.NewWriter(os.Stdout)
+
+	table.SetHeader([]string{"Name", "Score"})
+	for _, user := range users {
+		table.Append([]string{user.name, fmt.Sprintf("%v", user.score)})
+	}
+
+	table.Render()
+}
