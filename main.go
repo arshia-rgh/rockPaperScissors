a9c3c2ff561c2dd97050ccf5f1e87be0c75068c1
arshia <arshiarezagholi1212@gmail.com>
2024-09-17 22:01:45 +0330

    added start game menue to the main function

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
 	for {
 		selected := mainMenu()
 
 		if selected == 1 {
+
+			selectedInStartMenue := startGameMenu()
+
+			if selectedInStartMenue == 1 {
+				gameAi()
+				fmt.Println("Please enter anything to redirect... ")
+				_, err := fmt.Scan()
+				if err != nil {
+					return
+				}
+
+			} else if selectedInStartMenue == 2 {
+				gamePlayer()
+				fmt.Println("Please select anything to redirect ...")
+				_, err := fmt.Scan()
+				if err != nil {
+					return
+				}
+
+			} else {
+				continue
+			}
 
 		} else if selected == 2 {
 
