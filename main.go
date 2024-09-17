b4258e843c2b0cf536b563110ffa37415dd89fe7
arshia <arshiarezagholi1212@gmail.com>
2024-09-17 21:49:47 +0330

    basic implemention of main menu function in main func

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
 func main() {
+	for {
+		selected := mainMenu()
 
+		if selected == 1 {
+
+		} else if selected == 2 {
+
+		} else if selected == 3 {
+
+		} else if selected == 4 {
+
+		} else {
+
+		}
+	}
 }
