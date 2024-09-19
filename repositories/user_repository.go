86a38e5d01a226d8d6a2ed0bc56e125530070aab
arshia <arshiarezagholi1212@gmail.com>
2024-09-20 00:50:15 +0330

    added some user repository

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,1 +1,28 @@
 package repositories
+
+import (
+	"gorm.io/gorm"
+	"rockPaperScissors/models"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{
+		db,
+	}
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	return r.DB.Create(user).Error
+}
+
+func (r *UserRepository) GetUserByID(id uint) (*models.User, error) {
+	var user models.User
+
+	err := r.DB.First(&user, id).Error
+
+	return &user, err
+}
