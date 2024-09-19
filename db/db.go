73d28276e7694c324d548c29e4cb74b92462b9d1
arshia <arshiarezagholi1212@gmail.com>
2024-09-19 21:25:46 +0330

    completed ConnectDatabase function

--- db/db.go
+++ db/db.go
@@ -1,11 +1,37 @@
 package db
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbHost := os.Getenv("DB_HOST")
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the db: ", err)
+	}
+
+	DB = database
 
 }
