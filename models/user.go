9d524db20cc83f5c1ecdeda02db2504af34b6227
arshia <arshiarezagholi1212@gmail.com>
2024-09-22 19:05:16 +0330

    made User ID autoIncrement

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey"`
+	ID        uint      `gorm:"primaryKey;autoIncrement"`
 	Name      string    `gorm:"size:100"`
 	Score     int       `gorm:"default:0"`
 	CreatedAt time.Time `gorm:"default:autoCreateTime"`
