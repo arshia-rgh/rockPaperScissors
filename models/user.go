838ccf4bd06a7b0990daf1b5063731e49091e1c8
arshia <arshiarezagholi1212@gmail.com>
2024-09-22 18:18:09 +0330

    added NewUser instructer function for User struct

--- models/user.go
+++ models/user.go
@@ -8,3 +8,13 @@ type User struct {
 	Score     int       `gorm:"default:0"`
 	CreatedAt time.Time `gorm:"default:autoCreateTime"`
 }
+
+func NewUser(id uint, name string, score int) *User {
+	return &User{
+		ID:        id,
+		Name:      name,
+		Score:     score,
+		CreatedAt: time.Now(),
+	}
+
+}
