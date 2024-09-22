7054594fb972f7f16804ad949f32c33f5704e7ea
arshia <arshiarezagholi1212@gmail.com>
2024-09-22 20:45:30 +0330

    modified userExists utils to have repository

--- utils.go
+++ utils.go
@@ -4,13 +4,19 @@ func isValidName(name string) bool {
 	return name != ""
 }
 
-func userExists(name string) bool {
+func userExists(name string) (bool, error) {
+	users, err := userRepository.GetAllUsers()
+
+	if err != nil {
+		return false, err
+	}
+
 	for _, u := range users {
-		if u.name == name {
-			return true
+		if u.Name == name {
+			return true, nil
 		}
 	}
 
-	return false
+	return false, nil
 
 }
