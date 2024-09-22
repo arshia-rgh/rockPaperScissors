859ef9bfb7d858f21cf60af28a13c3c4f1c44661
arshia <arshiarezagholi1212@gmail.com>
2024-09-22 17:57:40 +0330

    changed migratiopn files to use model

--- db/migrations/create_user_table.go
+++ db/migrations/create_user_table.go
@@ -1,13 +1,11 @@
 package migrations
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"rockPaperScissors/models"
+)
 
 func CreateUsersTable(db *gorm.DB) error {
-	type User struct {
-		ID    uint   `gorm:"primaryKey"`
-		Name  string `gorm:"size:100"`
-		Score int    `gorm:"default:0"`
-	}
 
-	return db.AutoMigrate(&User{})
+	return db.AutoMigrate(&models.User{})
 }
