2099a01d21667764fcb10d714a59af3e108a6659
arshia <arshiarezagholi1212@gmail.com>
2024-09-22 17:59:11 +0330

    changed User struct

--- models/user.go
+++ models/user.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 type User struct {
-	ID    uint   `gorm:"primaryKey"`
-	Name  string `gorm:"size:100"`
-	Score int    `gorm:"default:0"`
+	ID        uint      `gorm:"primaryKey"`
+	Name      string    `gorm:"size:100"`
+	Score     int       `gorm:"default:0"`
+	CreatedAt time.Time `gorm:"default:autoCreateTime"`
 }
